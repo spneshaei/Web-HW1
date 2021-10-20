768b3703ca9a588b9996c2c26220caa4a54baa9d
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-10-20 18:37:22 +0330

    Index page design

---
+++ front/index.js
@@ -0,0 +1,6 @@
+indexInput = document.getElementById("indexInput");
+hashInput = document.getElementById("hashInput");
+
+function findString() {
+    const response = await fetch('http://localhost/api/names');
+}
