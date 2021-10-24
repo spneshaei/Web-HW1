4719cafffae31c6f2572d0ad1d540849ffac4bd0
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-10-24 11:12:37 +0330

    Locust

---
+++ locust/getnode/locustfile.py
@@ -0,0 +1,19 @@
+import string
+import random
+import time
+from locust import HttpUser, task, between
+
+class StringFinderUser(HttpUser):
+        def on_start(self):
+                self.client.post("/node/sha", json={"string":"hello123"})
+
+        @task
+        def findString(self):
+                self.client.get(f"/node/sha?sha256=27cc6994fc1c01ce6659c6bddca9b69c4c6a9418065e612c69d110b3f7b11f8a", name="/go/sha")
+                
+
+
+
+
+
+
